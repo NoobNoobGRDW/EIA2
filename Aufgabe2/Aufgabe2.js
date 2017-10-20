2c8de174bc4977eb0ddf5f4a1822ca6bef42bbbd
Linus <Linus@Surface.eduroam.hs-furtwangen.de>
2017-10-20 10:34:22 +0200

    10:34

---
+++ Aufgabe2/Aufgabe2.js
@@ -0,0 +1,129 @@
+/*
+Aufgabe: 2
+Name: Kuenz,Linus
+Matrikel: 256331
+Datum: 20.10.17
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+var Aufgabe2;
+(function (Aufgabe2) {
+    window.addEventListener("load", init);
+    function init() {
+        let canvas = document.getElementsByTagName("canvas")[0];
+        console.log(canvas);
+        let crc2 = canvas.getContext("2d");
+        console.log(crc2);
+        //Hintergrund
+        crc2.fillStyle = "#ADE0F3";
+        crc2.fillRect(0, 0, 800, 600);
+        //Berg1
+        crc2.beginPath();
+        crc2.moveTo(0, 600);
+        crc2.lineTo(600, 600);
+        crc2.lineTo(300, 50);
+        crc2.closePath();
+        crc2.strokeStyle = "#F1F7FA";
+        crc2.stroke();
+        crc2.fillStyle = "#F1F7FA";
+        crc2.fill();
+        //Berg2
+        crc2.beginPath();
+        crc2.moveTo(400, 600);
+        crc2.lineTo(800, 600);
+        crc2.lineTo(600, 150);
+        crc2.closePath();
+        crc2.strokeStyle = "#F1F7FA";
+        crc2.stroke();
+        crc2.fillStyle = "#F1F7FA";
+        crc2.fill();
+        //Sonne
+        crc2.beginPath();
+        crc2.arc(100, 90, 60, 0, 2 * Math.PI);
+        crc2.fillStyle = "#ffff02";
+        crc2.fill();
+        //Bergschatten1
+        crc2.beginPath();
+        crc2.moveTo(500, 600);
+        crc2.lineTo(600, 600);
+        crc2.lineTo(300, 50);
+        crc2.closePath();
+        crc2.strokeStyle = "#ABABAB";
+        crc2.stroke();
+        crc2.fillStyle = "#ABABAB";
+        crc2.fill();
+        //Bergschatten2
+        crc2.beginPath();
+        crc2.moveTo(570, 600);
+        crc2.lineTo(800, 600);
+        crc2.lineTo(600, 150);
+        crc2.closePath();
+        crc2.strokeStyle = "#ABABAB";
+        crc2.stroke();
+        crc2.fillStyle = "#ABABAB";
+        crc2.fill();
+        //Boden
+        crc2.fillStyle = "#F1F7FA";
+        crc2.fillRect(0, 290, 800, 450);
+        //Wolke
+        crc2.beginPath();
+        crc2.arc(650, 90, 40, 0, 2 * Math.PI);
+        crc2.fillStyle = "#F1F7FA";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(680, 90, 30, 0, 2 * Math.PI);
+        crc2.fillStyle = "#F1F7FA";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(620, 90, 30, 0, 2 * Math.PI);
+        crc2.fillStyle = "#F1F7FA";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(710, 90, 20, 0, 2 * Math.PI);
+        crc2.fillStyle = "#F1F7FA";
+        crc2.fill();
+        //H�tte
+        crc2.fillStyle = "#80400C";
+        crc2.fillRect(250, 300, 100, 40);
+        //Dach
+        crc2.beginPath();
+        crc2.moveTo(240, 300);
+        crc2.lineTo(360, 300);
+        crc2.lineTo(300, 260);
+        crc2.closePath();
+        crc2.strokeStyle = "#ABABAB";
+        crc2.stroke();
+        crc2.fillStyle = "#80400C";
+        crc2.fill();
+        //Lift.rechts
+        crc2.beginPath();
+        crc2.moveTo(500, 600);
+        crc2.lineTo(300, 300);
+        crc2.lineTo(300, 300);
+        crc2.strokeStyle = "#585858";
+        crc2.stroke();
+        //Lift.links
+        crc2.beginPath();
+        crc2.moveTo(500, 620);
+        crc2.lineTo(300, 320);
+        crc2.lineTo(300, 320);
+        crc2.strokeStyle = "#585858";
+        crc2.stroke();
+        for (let i = 0; i < 10; i++) {
+            let x = 150 + Math.random() * 100;
+            let y = 200 + Math.random() * 60;
+            drawTriangle(x, y, "#165118");
+        }
+        function drawTriangle(_x, _y, _color) {
+            //Baum
+            crc2.beginPath();
+            crc2.moveTo(_x, _y);
+            crc2.lineTo(_x + 20, _y + 30);
+            crc2.lineTo(_x - 20, _y + 30);
+            crc2.closePath();
+            crc2.stroke();
+            crc2.fillStyle = _color;
+            crc2.fill();
+        }
+    }
+})(Aufgabe2 || (Aufgabe2 = {}));
