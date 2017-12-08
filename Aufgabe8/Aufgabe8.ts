351df9d8e47cb0da028f39407226bc59419ced8b
Linus <Linus@Surface.eduroam.hs-furtwangen.de>
2017-12-08 10:51:07 +0100

    ..

--- Aufgabe8/Aufgabe8.ts
+++ Aufgabe8/Aufgabe8.ts
@@ -17,16 +17,10 @@ namespace Aufgabe_8 {
         var Number: string = prompt("Anzahl zwischen 10 und 100 angeben");
         var numberNum: number = parseInt(Number);               //ParseInt wandelt einen String in eine Zahl um
 
-        var Height: string = prompt("Gewünschte Höhe angeben");
-        var heightNum: number = parseInt(Height);
-
-        var Width: string = prompt("Gewünschte Breite angeben");
-        var widthNum: number = parseInt(Width);
-
 
         if (numberNum >= 10 && numberNum <= 100) {              //Prüft ob die Zahl zwischen 10 und 100 liegt
             for (var i: number = 0; i < numberNum; i++) {
-                drawRandom(Math.random() * 800, Math.random() * 600, "hsl(" + Math.random() * 360 + ", 100%, 50%)", heightNum, widthNum); //Übergibt x, y und Farbe
+                drawRandom(Math.random() * 800, Math.random() * 600, "hsl(" + Math.random() * 360 + ", 100%, 50%)"); //Übergibt x, y und Farbe
             }
         }
         else {
@@ -39,8 +33,8 @@ namespace Aufgabe_8 {
     function drawRandom(_x: number, _y: number, _color: string, _width: number, _height: number): void {
 
         let div: HTMLDivElement = document.createElement("div");        //erstellt Div
-        div.style.width = _width + "px";                                //Returns a String of an object
-        div.style.height = _height + "px";
+        div.style.width = 40 + "px";                                //Returns a String of an object
+        div.style.height = 40 + "px";
         div.style.marginLeft = _x + "px";                               //_x in einen String umwandeln, da h.style nur strings entgegen nehmen kann
         div.style.marginTop = _y + "px";
         div.style.backgroundColor = _color;                             //zufällige Farbe
