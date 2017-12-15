94918bcaa9ce2fea486e7fc7bdc217af509caf77
Linus <Linus@Surface.fritz.box>
2017-12-15 09:08:13 +0100

    Start

---
+++ Aufgabe9/Aufgabe9.ts
@@ -0,0 +1,8 @@
+/* 
+Aufgabe: 9
+Name: Kuenz,Linus
+Matrikel: 256331
+Datum: 14.12.17
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
