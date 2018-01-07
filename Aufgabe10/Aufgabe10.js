ea2d7eafb43c902abc456856573dc26e85ac9325
Linus <Linus@Surface.fritz.box>
2018-01-07 15:26:58 +0100

    upload

---
+++ Aufgabe10/Aufgabe10.js
@@ -0,0 +1,8 @@
+/*
+Aufgabe: 10
+Name: Kuenz,Linus
+Matrikel: 256331
+Datum: 03.01.2018
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/ 
