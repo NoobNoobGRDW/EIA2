755fda53165e3796984824bb4ec08cfcdccdbaa2
Linus <Linus@Surface.Speedport_W_724V_Typ_A_05011603_05_028>
2018-01-24 16:07:11 +0100

    dg

--- Aufgabe10/Artikel.js
+++ Aufgabe10/Artikel.js
@@ -9,94 +9,93 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 var Aufgabe10;
 (function (Aufgabe10) {
     //Baumarten
-    Aufgabe10.products = [
-        {
-            name: "Fichte",
-            price: 25,
-            typ: "tree",
-        },
-        {
-            name: "Weisstanne",
-            price: 30,
-            typ: "tree",
-        },
-        {
-            name: "Gruentanne",
-            price: 20,
-            typ: "tree",
-        },
-        {
-            name: "Nordmanntanne",
-            price: 35,
-            typ: "tree",
-        },
-        //Schmuckarten
-        {
-            name: "Rote Kugeln",
-            price: 3,
-            typ: "deko",
-        },
-        {
-            name: "Weisse Kugeln",
-            price: 4,
-            typ: "deko",
-        },
-        {
-            name: "Weihnachtsstern",
-            price: 15,
-            typ: "deko",
-        },
-        {
-            name: "Rote Kerzen",
-            price: 2,
-            typ: "deko",
-        },
-        {
-            name: "Weisse Kerzen",
-            price: 1,
-            typ: "deko",
-        },
-        {
-            name: "Blaue Kerzen",
-            price: 3,
-            typ: "deko",
-        },
-        //F��e
-        {
-            name: "50cm Fuss",
-            price: 30,
-            typ: "feet",
-        },
-        {
-            name: "40cm Fuss",
-            price: 25,
-            typ: "feet",
-        },
-        {
-            name: "30cm Fuss",
-            price: 20,
-            typ: "feet",
-        },
-        {
-            name: "20cm Fuss",
-            price: 15,
-            typ: "feet",
-        },
-        //Lieferung
-        {
-            name: "Standardversand",
-            typ: "shipping",
-            price: 5,
-        },
-        {
-            name: "Premiumversand",
-            typ: "shipping",
-            price: 10,
-        },
-        {
-            name: "Selbstabholung",
-            typ: "shipping",
-            price: 0,
-        },
-    ];
+    Aufgabe10.fichte = {
+        name: "Fichte",
+        price: 25,
+        typ: "tree",
+    };
+    Aufgabe10.weisstanne = {
+        name: "Weisstanne",
+        price: 30,
+        typ: "tree",
+    };
+    Aufgabe10.gruentanne = {
+        name: "Gruentanne",
+        price: 20,
+        typ: "tree",
+    };
+    Aufgabe10.nordmann = {
+        name: "Nordmanntanne",
+        price: 35,
+        typ: "tree",
+    };
+    //Schmuckarten
+    Aufgabe10.kugelRot = {
+        name: "Rote Kugeln",
+        price: 3,
+        typ: "deko",
+    };
+    Aufgabe10.kugelWeiss = {
+        name: "Weisse Kugeln",
+        price: 4,
+        typ: "deko",
+    };
+    Aufgabe10.stern = {
+        name: "Weihnachtsstern",
+        price: 15,
+        typ: "deko",
+    };
+    Aufgabe10.kerzeRot = {
+        name: "Rote Kerzen",
+        price: 2,
+        typ: "deko",
+    };
+    Aufgabe10.kerzeWeiss = {
+        name: "Weisse Kerzen",
+        price: 1,
+        typ: "deko",
+    };
+    Aufgabe10.kerzeBlau = {
+        name: "Blaue Kerzen",
+        price: 3,
+        typ: "deko",
+    };
+    //F��e
+    Aufgabe10.fussGross = {
+        name: "50cm Fuss",
+        price: 30,
+        typ: "feet",
+    };
+    Aufgabe10.fussMittel = {
+        name: "40cm Fuss",
+        price: 25,
+        typ: "feet",
+    };
+    Aufgabe10.fussKlein = {
+        name: "30cm Fuss",
+        price: 20,
+        typ: "feet",
+    };
+    Aufgabe10.fig = {
+        name: "20cm Fuss",
+        price: 15,
+        typ: "feet",
+    };
+    //Lieferung
+    Aufgabe10.standard = {
+        name: "Standardversand",
+        typ: "shipping",
+        price: 5,
+    };
+    Aufgabe10.premium = {
+        name: "Premiumversand",
+        typ: "shipping",
+        price: 10,
+    };
+    Aufgabe10.abhol = {
+        name: "Selbstabholung",
+        typ: "shipping",
+        price: 0,
+    };
+    Aufgabe10.arrayArtikel = [Aufgabe10.fichte, Aufgabe10.weisstanne, Aufgabe10.gruentanne, Aufgabe10.nordmann, Aufgabe10.kugelRot, Aufgabe10.kugelWeiss, Aufgabe10.stern, Aufgabe10.kerzeRot, Aufgabe10.kerzeWeiss, Aufgabe10.kerzeBlau];
 })(Aufgabe10 || (Aufgabe10 = {}));
--- Aufgabe10/Artikel.ts
+++ Aufgabe10/Artikel.ts
@@ -10,109 +10,108 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 
 namespace Aufgabe10 {
 
-    export interface Artikel {
+    export interface Products {
         name: string;
         price: number;
         typ: string;
     }
 
     //Baumarten
-    export let products: Artikel[] = [
-        {
-            name: "Fichte",
-            price: 25,
-            typ: "tree",
-        },
-        {
-            name: "Weisstanne",
-            price: 30,
-            typ: "tree",
-        },
-        {
-            name: "Gruentanne",
-            price: 20,
-            typ: "tree",
-        },
-        {
-            name: "Nordmanntanne",
-            price: 35,
-            typ: "tree",
-        },
+    export let fichte: Products = {
+        name: "Fichte",
+        price: 25,
+        typ: "tree",
+    };
+    export let weisstanne: Products = {
+        name: "Weisstanne",
+        price: 30,
+        typ: "tree",
+    };
+    export let gruentanne: Products = {
+        name: "Gruentanne",
+        price: 20,
+        typ: "tree",
+    };
+    export let nordmann: Products = {
+        name: "Nordmanntanne",
+        price: 35,
+        typ: "tree",
+    };
 
 
-        //Schmuckarten
-        {
-            name: "Rote Kugeln",
-            price: 3,
-            typ: "deko",
-        },
-        {
-            name: "Weisse Kugeln",
-            price: 4,
-            typ: "deko",
-        },
-        {
-            name: "Weihnachtsstern",
-            price: 15,
-            typ: "deko",
-        },
-        {
-            name: "Rote Kerzen",
-            price: 2,
-            typ: "deko",
-        },
-        {
-            name: "Weisse Kerzen",
-            price: 1,
-            typ: "deko",
-        },
-        {
-            name: "Blaue Kerzen",
-            price: 3,
-            typ: "deko",
-        },
+    //Schmuckarten
+    export let kugelRot: Products = {
+        name: "Rote Kugeln",
+        price: 3,
+        typ: "deko",
+    };
+    export let kugelWeiss: Products = {
+        name: "Weisse Kugeln",
+        price: 4,
+        typ: "deko",
+    };
+    export let stern: Products = {
+        name: "Weihnachtsstern",
+        price: 15,
+        typ: "deko",
+    };
+    export let kerzeRot: Products = {
+        name: "Rote Kerzen",
+        price: 2,
+        typ: "deko",
+    };
+    export let kerzeWeiss: Products = {
+        name: "Weisse Kerzen",
+        price: 1,
+        typ: "deko",
+    };
+    export let kerzeBlau: Products = {
+        name: "Blaue Kerzen",
+        price: 3,
+        typ: "deko",
+    };
 
 
-        //Füße
-        {
-            name: "50cm Fuss",
-            price: 30,
-            typ: "feet",
-        },
-        {
-            name: "40cm Fuss",
-            price: 25,
-            typ: "feet",
-        },
-        {
-            name: "30cm Fuss",
-            price: 20,
-            typ: "feet",
-        },
-        {
-            name: "20cm Fuss",
-            price: 15,
-            typ: "feet",
-        },
+    //Füße
+    export let fussGross: Products = {
+        name: "50cm Fuss",
+        price: 30,
+        typ: "feet",
+    };
+    export let fussMittel: Products = {
+        name: "40cm Fuss",
+        price: 25,
+        typ: "feet",
+    };
+    export let fussKlein: Products = {
+        name: "30cm Fuss",
+        price: 20,
+        typ: "feet",
+    };
+    export let fig: Products = {
+        name: "20cm Fuss",
+        price: 15,
+        typ: "feet",
+    };
 
-        //Lieferung
-        {
-            name: "Standardversand",
-            typ: "shipping",
-            price: 5,
-        },
+    //Lieferung
+    export let standard: Products = {
+        name: "Standardversand",
+        typ: "shipping",
+        price: 5,
+    };
 
-        {
-            name: "Premiumversand",
-            typ: "shipping",
-            price: 10,
-        },
+    export let premium: Products = {
+        name: "Premiumversand",
+        typ: "shipping",
+        price: 10,
+    };
 
-        {
-            name: "Selbstabholung",
-            typ: "shipping",
-            price: 0,
-        },
-    ];
+    export let abhol: Products = {
+        name: "Selbstabholung",
+        typ: "shipping",
+        price: 0,
+    };
 
+export var arrayArtikel = [fichte, weisstanne, gruentanne, nordmann, kugelRot, kugelWeiss, stern, kerzeRot, kerzeWeiss, kerzeBlau];
 }
