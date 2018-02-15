6a32745e9e258b76c1d420722091c642ca597681
Linus <Linus@Surface.fritz.box>
2018-02-15 15:22:14 +0100

    g

--- Semesteraufgabe/Semesteraufgabe1.js
+++ Semesteraufgabe/Semesteraufgabe1.js
@@ -117,7 +117,7 @@ var Semesteraufgabe1;
         Semesteraufgabe1.crc2.fillRect(140, 410, 8, 200);
         //Platte
         Semesteraufgabe1.crc2.fillStyle = "#945415";
-        Semesteraufgabe1.crc2.fillRect(140, 520, 40, 8);
+        Semesteraufgabe1.crc2.fillRect(140, 520, 80, 8);
         canImg = Semesteraufgabe1.crc2.getImageData(0, 0, 1000, 600); //speichert das Canvas Image
         animiere(); //f�hrt Funktion aus
     }
