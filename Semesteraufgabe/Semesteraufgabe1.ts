a3dcd6b57dc515f5829e6e7ae5b92e8a684fdf74
Linus <Linus@Surface.fritz.box>
2018-02-15 15:26:26 +0100

    n

--- Semesteraufgabe/Semesteraufgabe1.ts
+++ Semesteraufgabe/Semesteraufgabe1.ts
@@ -140,13 +140,13 @@ namespace Semesteraufgabe1 {
         
         //Stuhl rechts
         crc2.fillStyle = "#945415";
-        crc2.fillRect(790, 520, 8, 200);
+        crc2.fillRect(805, 520, 8, 200);
         //Platte
         crc2.fillStyle = "#945415";
-        crc2.fillRect(790, 520, 100, 8);
+        crc2.fillRect(805, 520, 100, 8);
         //Bein rechts
         crc2.fillStyle = "#945415";
-        crc2.fillRect(890, 410, 8, 200);
+        crc2.fillRect(905, 410, 8, 200);
         
         canImg = crc2.getImageData(0, 0, 1000, 600); //speichert das Canvas Image
 
