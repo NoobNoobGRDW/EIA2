76655bbbafd4729842e0c2e60d68e0af6da92fab
Linus <Linus@Surface.fritz.box>
2018-02-15 15:43:24 +0100

    dfg

--- Semesteraufgabe/Semesteraufgabe1.js
+++ Semesteraufgabe/Semesteraufgabe1.js
@@ -172,9 +172,9 @@ var Semesteraufgabe1;
         Semesteraufgabe1.crc2.fill();
         //Stuhl
         Semesteraufgabe1.crc2.fillStyle = "#0F0F0F";
-        Semesteraufgabe1.crc2.fillRect(350, 320, 15, 100);
+        Semesteraufgabe1.crc2.fillRect(350, 350, 15, 50);
         Semesteraufgabe1.crc2.fillStyle = "#ADACAB";
-        Semesteraufgabe1.crc2.fillRect(350, 320, 2, 100);
+        Semesteraufgabe1.crc2.fillRect(350, 350, 2, 50);
         canImg = Semesteraufgabe1.crc2.getImageData(0, 0, 1000, 600); //speichert das Canvas Image
         animiere(); //f�hrt Funktion aus
     }
