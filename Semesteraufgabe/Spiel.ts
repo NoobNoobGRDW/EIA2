e34a0e456a58da348d0efe56cf1513b00468e203
Linus <Linus@Surface.fritz.box>
2018-02-15 19:39:35 +0100

    k

--- Semesteraufgabe/Spiel.ts
+++ Semesteraufgabe/Spiel.ts
@@ -225,10 +225,10 @@ namespace Spiel {
 
         //schwarze Tasten von links
         crc2.beginPath();
-        crc2.moveTo(180, 480);//links 
+        crc2.moveTo(178, 480);//links 
         crc2.lineTo(205, 480);//rechts
-        crc2.lineTo(215, 420);//oben rechts
-        crc2.lineTo(195, 420);//oben links   
+        crc2.lineTo(217, 420);//oben rechts
+        crc2.lineTo(193, 420);//oben links   
         crc2.closePath();
         crc2.strokeStyle = "#ADACAB";
         crc2.stroke();
