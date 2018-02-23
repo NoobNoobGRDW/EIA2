7575c1f2ba1e6bbdeb3693d005bd64d9d7eee92d
Linus <Linus@Surface>
2018-02-23 15:58:08 +0100

    fg

--- Semesteraufgabe/Lampen.ts
+++ Semesteraufgabe/Lampen.ts
@@ -38,8 +38,7 @@ namespace Semesteraufgabe1 {
             crc2.stroke();
             crc2.fillStyle = this.color;
             crc2.fill();
-            crc2.arc(this.x + 10, this.y + 10, 10, 0, 2 * Math.PI);
-            crc2.fillStyle = "#000000";
+
             //Kreis rechts
             crc2.beginPath();
             crc2.arc(this.x + 900, this.y, 35, 0, 2 * Math.PI);
@@ -47,8 +46,7 @@ namespace Semesteraufgabe1 {
             crc2.stroke();
             crc2.fillStyle = this.color;
             crc2.fill();
-            crc2.arc(this.x + 10, this.y + 10, 10, 0, 2 * Math.PI);
-            crc2.fillStyle = "#000000";
+
 
 
         }
